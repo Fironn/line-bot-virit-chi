b399a7ab28d8fd160fdaab7df8ea787331d5755d
Fironn <shihomihashi@gmail.com>
2019-06-25 15:16:03 +0900

    [CHA]to be hiragana & detect lang

---
+++ toHiragana.py
@@ -0,0 +1,28 @@
+from goolabs import GoolabsAPI
+import json
+from langdetect import detect
+
+
+codeUrl="gooToken.json"
+f = open(codeUrl, 'r')
+
+codeData = json.load(f)
+f.close()
+
+app_id = str(codeData["App-Id"])
+api = GoolabsAPI(app_id)
+
+def toHiragana(sentence):
+    res=api.hiragana(
+       request_id="hiragana-req002",
+       sentence=sentence,
+       output_type="hiragana" # hiragana or katakana
+    )
+    print(res)
+    return res["converted"].strip(' ').strip(' ')
+
+def reqLang(sentence):
+    lang = detect(sentence)
+    print(lang)
+    return lang
+
